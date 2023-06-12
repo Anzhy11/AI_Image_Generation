dc73d6b4b5d488a9edd69702a1b1953a60111764
Aminat27 <adebayoaminat27@gmail.com>
2023-06-12 11:22:10 +0100

    fixed screen height

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
           Create
         </Link>
       </header>
-      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[100vh]">
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-post" element={<CreatePost />} />

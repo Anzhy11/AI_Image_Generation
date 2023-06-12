cee0bd7123ab0f80f4de8f936c4177b1836a2dbe
Aminat27 <adebayoaminat27@gmail.com>
2023-06-12 11:07:50 +0100

    fix some issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
         <Link to="/">
           <img src={logo} alt="logo" className="w-28 object-contain"/>
         </Link>
-        <Link to="creat-post" className="font-inter font-medium  bg-[#47afdc] text-white px-4 py-2 rounded-md">
+        <Link to="/create-post" className="font-inter font-medium  bg-[#47afdc] text-white px-4 py-2 rounded-md">
           Create
         </Link>
         

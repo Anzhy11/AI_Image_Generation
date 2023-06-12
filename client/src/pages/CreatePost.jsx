58c628a21fce4176e319e6587da561139bb3641a
Aminat27 <adebayoaminat27@gmail.com>
2023-06-12 14:04:52 +0100

    added search in Home

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState }from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  cons
   return <div>CreatePost</div>;
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,11 @@ import { useState, useEffect } from "react";
 import { Loader, Card, FormField } from "../components";
 const RenderCards = ({ data, title }) => {
   if(data?.length > 0){
-    return data.map((post)) => <Card key={post._id} {...post}
+    return data.map((post) => <Card key={post._id} {...post} />)
   } 
+  return (
+    <h2 className="mt-5 font-bold text-[#47afdc] text-xl uppercase">{title}</h2>
+  )
 }
 
 const Home = () => {
@@ -34,15 +37,21 @@ const Home = () => {
             </h2>
           )}
           <div className="grid lg:cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
-
+            {searchText ? (
+              <RenderCards
+                data={[]}
+                title="No search results found"
+              />
+            ) : (
+              <RenderCards
+                data={[]}
+                title="No posts found"
+              />
+            )}
           </div>
         </>
-      )
-
-      }
-
+      )}
     </div>
-
   </section>
   )
 };

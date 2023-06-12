885bdde813a0f3afd72cd9c5142b853a1e9d3547
Anzhy11 <harnzzy@gmail.com>
2023-06-12 19:31:30 +0100

    fixed open ai import error

--- server/controllers/dalleController.js
+++ server/controllers/dalleController.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv').config();
-const { Configuration, OpenAIApi } = require('OpenAI')
+const { Configuration, OpenAIApi } = require('openai')
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,

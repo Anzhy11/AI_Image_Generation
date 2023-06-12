9eef3a451f10d1e7cb8fb1e2795edbbea9c6376a
Anzhy11 <harnzzy@gmail.com>
2023-06-12 11:36:45 +0100

    fixed some merge issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,23 @@ import { Home, CreatePost } from "./pages";
 const App = () => {
   return (
     <>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 border-b border-b-[#e6ebf4]">
-        Header
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
+        <Link
+          to="/create-post"
+          className="font-inter font-medium  bg-[#47afdc] text-white px-4 py-2 rounded-md"
+        >
+          Create
+        </Link>
       </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-post" element={<CreatePost />} />
-      </Routes>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[100vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
       </main>
-      
     </>
   );
 };

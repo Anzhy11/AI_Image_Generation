e292be48598d3b7b69afff647a54b0a4e80752ad
Anzhy11 <harnzzy@gmail.com>
2023-06-12 14:52:15 +0100

    added post routes and controller

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,8 +1,34 @@
 const dotenv = require('dotenv').config();
+const { v2, cloudinary } = require('cloudinary');
 
+const Post = require('../models/post');
 
-const checkAPI = async (req, res) => {
+cloudinary.config({
+  cloud_name: process.env.CLOUDINRY_CLOUD_NAME,
+  api_key: process.env.CLOUDINRY_API_KEY,
+  api_key_secret: process.env.CLOUDINRY_API_SECRET,
+});
+
+const getAllPosts = async (req, res) => {
   res.send('hallo there')
 }
 
-module.exports = { checkAPI };
+const createPost = async (req, res) => {
+  const { name, prompt, photo } = req.body;
+
+  try {
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    })
+
+    res.status(201).json({ success: true, data: newPost });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+}
+
+module.exports = { getAllPosts, createPost };
--- server/routes/postsRoutes.js
+++ server/routes/postsRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { checkAPI } = require('../controllers/postController')
+const { getAllPosts, createPost } = require('../controllers/postController')
 
-router.route('/').get(checkAPI);
+
+router.route('/').get(getAllPosts);
+router.route('/').post(createPost);
+
 
 
 module.exports = router;

72fa9623dc77087b8c70157669838bc8ddd4e1a1
Aminat27 <adebayoaminat27@gmail.com>
2023-06-12 11:18:53 +0100

    added main section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,12 +13,14 @@ const App = () => {
         <Link to="/create-post" className="font-inter font-medium  bg-[#47afdc] text-white px-4 py-2 rounded-md">
           Create
         </Link>
-        
       </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[100vh]">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-post" element={<CreatePost />} />
       </Routes>
+      </main>
+      
     </>
   );
 };

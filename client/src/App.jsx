e51aac09c1a7e6938e457247d7340c82c303f317
Anzhy11 <harnzzy@gmail.com>
2023-06-12 11:25:42 +0100

    added header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,22 @@ import { Home, CreatePost } from "./pages";
 const App = () => {
   return (
     <>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 border-b border-b-[#e6ebf4]">
-        Header
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
+
+        <Link
+          to="/create-post"
+          className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+        >
+          Create
+        </Link>
       </header>
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/create-post" element={<CreatePost />} />
       </Routes>
     </>
   );

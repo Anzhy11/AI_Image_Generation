bfa8762ae8bf57bb76d6159b6f9fa83785195a37
Anzhy11 <harnzzy@gmail.com>
2023-06-12 10:28:57 +0100

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,18 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { logo } from "./assets";
+import { Home, CreatePost } from "./pages";
+
 const App = () => {
   return (
-    <div className="flex justify-center items-center text-blue-300">App</div>
+    <>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 border-b border-b-[#e6ebf4]">
+        Header
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost />} />
+      </Routes>
+    </>
   );
 };
 

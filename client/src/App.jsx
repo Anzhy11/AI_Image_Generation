fb3199f63635fb370b33f6f8156ab8a7976935cb
Aminat27 <adebayoaminat27@gmail.com>
2023-06-12 11:05:04 +0100

    added header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+
 import { Routes, Route, Link } from "react-router-dom";
 import { logo } from "./assets";
 import { Home, CreatePost } from "./pages";
@@ -5,12 +6,18 @@ import { Home, CreatePost } from "./pages";
 const App = () => {
   return (
     <>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 border-b border-b-[#e6ebf4]">
-        Header
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain"/>
+        </Link>
+        <Link to="creat-post" className="font-inter font-medium  bg-[#47afdc] text-white px-4 py-2 rounded-md">
+          Create
+        </Link>
+        
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/create-post" element={<CreatePost />} />
       </Routes>
     </>
   );
